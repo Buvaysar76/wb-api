5df6fcae80d27b83033eea3c3c40b424962d4feb
Buvaysar <buvaysar2032@gmail.com>
2025-11-07 19:58:39 +0300

    feat: настроено ежедневное обновление данных дважды в день

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:import-api-data --type=orders')->twiceDaily(8, 20);
+Schedule::command('app:import-api-data --type=sales')->twiceDaily(8, 20);
+Schedule::command('app:import-api-data --type=stocks')->twiceDaily(8, 20);
+Schedule::command('app:import-api-data --type=incomes')->twiceDaily(8, 20);
